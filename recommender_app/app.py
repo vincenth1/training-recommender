9b8dd4b1da9dd495a0ba13533e25a87ee3340406
vincenth1 <vincent-h@live.de>
2025-07-11 11:39:13 +0200

    Update default port in app.py from 5000 to 5001 for application configuration

--- recommender_app/app.py
+++ recommender_app/app.py
@@ -318,5 +318,5 @@ def book_slot():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(debug=False, host="0.0.0.0", port=port)
